ad96991130d761101ef3e5b45dc25fb2a9dc434d
MuhammedCubukcu <muhammedcubukcu0@gmail.com>
2024-08-14 21:12:38 +0300

    Edit about.js

--- src/data.js
+++ src/data.js
@@ -2,6 +2,8 @@ import tour1 from "./images/tour-1.jpeg";
 import tour2 from "./images/tour-2.jpeg";
 import tour3 from "./images/tour-3.jpeg";
 import tour4 from "./images/tour-4.jpeg";
+import tour5 from "./images/tour-5.jpeg";
+import tour6 from "./images/tour-6.jpeg";
 
 export const pageLinks = [
   { id: 1, href: "#home", text: "home" },
@@ -52,7 +54,7 @@ export const tours = [
     id: 2,
     image: tour2,
     date: "october 1th, 2020",
-    title: "best of java",
+    title: "Bali",
     info: ` Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.`,
     location: "indonesia",
     duration: 11,
@@ -73,6 +75,26 @@ export const tours = [
     image: tour4,
     date: "december 5th, 2019",
     title: "kenya highlights",
+    info: ` Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.`,
+    location: "kenya",
+    duration: 20,
+    cost: 3300,
+  },
+  {
+    id: 5,
+    image: tour5,
+    date: "december 5th, 2019",
+    title: "x",
+    info: ` Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.`,
+    location: "kenya",
+    duration: 20,
+    cost: 3300,
+  },
+  {
+    id: 6,
+    image: tour6,
+    date: "december 5th, 2019",
+    title: "y",
     info: ` Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.`,
     location: "kenya",
     duration: 20,
